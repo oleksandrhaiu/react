658eab4fdd6afc5965b64fecd17486e6d64ab70d
Oleksadr Haiu <sashagayu1@gmail.com>
2025-11-26 16:42:19 +0200

    feat: add counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import {useState} from "react";
+
 function App() {
+  const [likes, setLikes] = useState(0)
+  console.log(likes);
+  console.log(setLikes);
 
   return (
     <div className="App">
+      <h1>{likes}</h1>
+      <button onClick={() => setLikes(likes + 1)}>Increment</button>
+      <button onClick={() => setLikes(likes - 1)}>Decrement</button>
     </div>
   )
 }
